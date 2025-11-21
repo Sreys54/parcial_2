204f1225c3bf73e87cacb37a4238ae7ff3dececb
Sreys54 <123739656+Sreys54@users.noreply.github.com>
2025-11-21 09:14:57 -0500

    Se crea el controller de auditorio no se resuleve el error de crear evento

--- src/auditorio/auditorio.controller.ts
+++ src/auditorio/auditorio.controller.ts
@@ -1,14 +1,4 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Put,
-  Delete,
-  Body,
-  Param,
-  HttpCode,
-  HttpStatus,
-} from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
 import { AuditorioService } from './auditorio.service';
 import { AuditorioEntity } from './auditorio.entity/auditorio.entity';
 
@@ -19,29 +9,5 @@ export class AuditorioController {
   @Post()
   async crear(@Body() auditorio: AuditorioEntity): Promise<AuditorioEntity> {
     return this.auditorioService.crearAuditorio(auditorio);
-  }
-
-  @Get()
-  async listar(): Promise<AuditorioEntity[]> {
-    return this.auditorioService.findAll();
-  }
-
-  @Get(':id')
-  async obtener(@Param('id') id: string): Promise<AuditorioEntity> {
-    return this.auditorioService.findAuditorioById(id);
-  }
-
-  @Put(':id')
-  async actualizar(
-    @Param('id') id: string,
-    @Body() datos: Partial<AuditorioEntity>,
-  ): Promise<AuditorioEntity> {
-    return this.auditorioService.actualizarAuditorio(id, datos);
-  }
-
-  @Delete(':id')
-  @HttpCode(HttpStatus.NO_CONTENT)
-  async eliminar(@Param('id') id: string): Promise<void> {
-    return this.auditorioService.eliminarAuditorio(id);
   }
 }
